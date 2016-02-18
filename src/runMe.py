a9200c767a9a5319ba85414eec020ff494479ecc
syedshabihhasan <hasanshabih@gmail.com>
2016-02-17 23:27:52 -0600

    made a tiny change

--- src/runMe.py
+++ src/runMe.py
@@ -29,7 +29,7 @@ def creategraph(data, isStatic = True):
         graph_obj.addedges(link_tuple)
         return links, link_tuple, graph_obj
     else:
-        start_datetime = dt.datetime.strptime('', '%Y-%m-%d %H:%M:%S')
+        start_datetime = dt.datetime.strptime('2014-12-11 00:00:00', '%Y-%m-%d %H:%M:%S')
         week_dict, link_tuple = hlp.getdynamiclinks(pid_dict, data, start_datetime)
         toWrite_edge, toWrite_node = graph_obj.exportdynamicgraph(link_tuple, pid_dict)
         return toWrite_edge, toWrite_node, week_dict
@@ -39,19 +39,19 @@ def main():
     print 'filtering...'
     sms_data = ff.filterbyequality(1, 'sms')
     print 'done'
-    if '~' is not sys.argv[2]:
+    if '-' is not sys.argv[2]:
         writecsv(sys.argv[2], sms_data)
-    if '~' is not sys.argv[3]:
+    if '-' is not sys.argv[3]:
         links, link_tuple, graph_obj = creategraph(sms_data)
         graph_obj.writegraph(sys.argv[3])
-    if '~' is not sys.argv[4]:
+    if '-' is not sys.argv[4]:
         toWrite_edge, toWrite_node, week_dict = creategraph(sms_data, False)
         writetofile(sys.argv[4]+'_el.csv', toWrite_edge)
         writetofile(sys.argv[4]+'_nl.csv', toWrite_node)
 
 if __name__ == "__main__":
     if not (5 == len(sys.argv)):
-        print 'Usage: python runMe.py <csv filename> <path for output filtered file, enter ~ for skipping> ' \
-              '<path for graph file, enter ~ for skipping> <path for dynamic graph, enter ~ for skipping>'
+        print 'Usage: python runMe.py <csv filename> <path for output filtered file, enter - for skipping> ' \
+              '<path for graph file, enter - for skipping> <path for dynamic graph, enter - for skipping>'
     else:
         main()
